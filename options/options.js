043663c5fbea8d2b96b69fc48f0a1710a3edfc1b
Crowdbotics <engineering@crowdbotics.com>
2023-06-09 23:19:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-union-dev-90087",
+  url: "https://orange-union-dev-90087.botics.co",
+  api: "https://orange-union-dev-90087.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
